d891b9731e2eccfd29aa86f9e492a67d81f6cb60
ostryaninam <anna.ostryanina@gmail.com>
2020-02-24 15:56:12 +0300

    wrote ienumerable for compressedfilereader

--- FileManagerLibrary/Implementations/CompressedFileReader.cs
+++ FileManagerLibrary/Implementations/CompressedFileReader.cs
@@ -5,27 +5,19 @@ using System.Text;
 using FileManagerLibrary.Abstractions;
 using ExceptionsHandling;
 using DataCollection;
+using System.Collections;
 
 namespace FileManagerLibrary.Implementations
 {
-    class CompressedFileReader : IFileReader
+    class CompressedFileIEnumerator : IEnumerator<DataBlock>
     {
-        FileStream fileStream;
-        int numberOfBlocks;
-        public CompressedFileReader(string path)
+        private readonly FileStream fileStream;
+
+        public CompressedFileIEnumerator(FileStream filestream)
         {
-            try
-            {
-                fileStream = new FileStream(path, FileMode.Open);
-                ReadNumberOfBlocks();
-            }
-            catch (IOException e)
-            {
-                ExceptionsHandler.Handle(this.GetType(),e);
-            }
+            fileStream = filestream;
         }
         public bool EndOfFile => fileStream.Position >= fileStream.Length;
-        public int NumberOfBlocks { get => numberOfBlocks; }
         public DataBlock ReadBlock()
         {
             var index = ReadIndex();
@@ -40,12 +32,6 @@ namespace FileManagerLibrary.Implementations
             fileStream.Read(byteLengthOfBlock, 0, 8);
             return BitConverter.ToInt64(byteLengthOfBlock, 0);
         }
-        private void ReadNumberOfBlocks()
-        {
-            var byteNumberOfBlocks = new byte[4];
-            fileStream.Read(byteNumberOfBlocks, 0, 4);
-            numberOfBlocks = BitConverter.ToInt32(byteNumberOfBlocks, 0);
-        }
 
         private int ReadInt32()
         {
@@ -53,9 +39,65 @@ namespace FileManagerLibrary.Implementations
             fileStream.Read(byteLengthOfBlock, 0, 4);
             return BitConverter.ToInt32(byteLengthOfBlock, 0);
         }
+
+        DataBlock IEnumerator<DataBlock>.Current => ReadBlock();
+
+        public object Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            return EndOfFile;
+        }
+
+        public void Reset()
+        {
+            fileStream.Position = 0;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+    class CompressedFileReader : IFileReader
+    {
+        private FileStream fileStream;
+        private int numberOfBlocks;
+        public CompressedFileReader(string path)
+        {
+            try
+            {
+                fileStream = new FileStream(path, FileMode.Open);
+                ReadNumberOfBlocks();
+            }
+            catch (IOException e)
+            {
+                ExceptionsHandler.Handle(this.GetType(),e);
+            }
+        }
+        public int NumberOfBlocks { get => numberOfBlocks; }
+
+        private void ReadNumberOfBlocks()
+        {
+            var byteNumberOfBlocks = new byte[4];
+            fileStream.Read(byteNumberOfBlocks, 0, 4);
+            numberOfBlocks = BitConverter.ToInt32(byteNumberOfBlocks, 0);
+        }
+
         public void Dispose()
         {
             fileStream.Close();
         }
+
+        public IEnumerator<DataBlock> GetEnumerator()
+        {
+            return new CompressedFileIEnumerator(fileStream);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerator<DataBlock>)GetEnumerator());
+        }
+
     }
 }
